bc6becd99f6d92e40e4ba7b45f4d83b098be4100
carpenter-js <carpenterbarrett@gmail.com>
2019-01-09 20:43:15 -0700

    small style changes

--- src/components/index.js
+++ src/components/index.js
@@ -22,7 +22,7 @@ export default function App(props) {
 						googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDM_ehj1epOAaYbWLgIG2vWT4ErYl-2PJg&v=3.exp&libraries=geometry,drawing,places"
 						loadingElement={<div style={{ height: `100%` }} />}
 						containerElement={<div style={{ height: `400px`, width: `100%` }} />}
-						mapElement={<div style={{ height: `100%` }} />}
+						mapElement={<div style={{ height: `100%`, borderRadius: '15px' }} />}
 					/>
 			</div>
 
